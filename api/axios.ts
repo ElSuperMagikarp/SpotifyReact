d5a7fcaab77978fdcf10d1b4fa0e1df3a87c6c36
ElSuperMagikarp <uriens2006@gmail.com>
2026-01-30 16:07:36 +0100

    Request Id provisional a axios

--- api/axios.ts
+++ api/axios.ts
@@ -3,4 +3,7 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: "http://localhost:5000",
     timeout: 5000,
+    params: {
+        requesterId: "99999999-9999-9999-9999-999999999999", // PROVISIONAL
+    },
 });
