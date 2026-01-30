28d294675cfb560a34dcf6130e3b2b823d78b4ad
ElSuperMagikarp <uriens2006@gmail.com>
2026-01-30 16:07:13 +0100

    PER FI FUNCIONA EL REACT

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,13 +5,13 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import MenuButton from "@/components/layout/MenuButton";
 
-import "../global.css";
+import "@/global.css";
 
 export default function RootLayout() {
   return (
     <SafeAreaView className="w-full h-full bg-regular flex-col-reverse sm:flex-row">
       {/* Menu */}
-      <View className=" bg-regular border-regular
+      <View className="bg-regular border-regular
         w-full h-[8%] border-t flex-row items-center justify-evenly
         sm:h-full sm:w-auto sm:min-w-[5%] sm:border-t-0 sm:border-r-2 sm:flex-col sm:px-5 sm:justify-start sm:py-10 gap-y-10">
         <MenuButton url="/profile" Icon={User} />
