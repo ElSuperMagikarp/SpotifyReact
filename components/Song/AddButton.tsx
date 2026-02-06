e0e0a7c4605c58767b50f43fa6665146d09650ec
ElSuperMagikarp <uriens2006@gmail.com>
2026-02-06 17:08:45 +0100

    optional classes to AddButton

--- components/Song/AddButton.tsx
+++ components/Song/AddButton.tsx
@@ -3,11 +3,12 @@ import { TouchableOpacity } from 'react-native';
 
 interface Props {
     size?: number;
+    className?: string;
 }
 
-const AddButton = ({ size }: Props) => {
+const AddButton = ({ size, className }: Props) => {
     return (
-        <TouchableOpacity className='flex flex-col justify-center items-center text-white'>
+        <TouchableOpacity className={`flex flex-col justify-center items-center text-white ${className ?? ''}`}>
             <Plus size={size ? size : 20}></Plus>
         </TouchableOpacity>
     )
