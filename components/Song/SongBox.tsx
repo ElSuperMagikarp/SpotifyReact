ae574dadcfe098f8866b0243161ebe9e2b45376c
ElSuperMagikarp <uriens2006@gmail.com>
2026-02-06 17:09:12 +0100

    AddButton to SongBox

--- components/Song/SongBox.tsx
+++ components/Song/SongBox.tsx
@@ -2,6 +2,8 @@ import { Music2 } from 'lucide-react-native';
 import { useState } from 'react';
 import { Image, Text, View } from 'react-native';
 
+import AddButton from './AddButton';
+
 interface Props {
     title: string;
     artist: string;
@@ -13,7 +15,7 @@ const SongBox = ({ title, artist, imageUrl }: Props) => {
 
     return (
         <View className='flex flex-col justify-center w-28 sm:w-44'>
-            <View className='w-full aspect-square bg-spotify-green rounded-md overflow-hidden flex justify-center items-center'>
+            <View className='group w-full aspect-square bg-gray-500 rounded-md overflow-hidden flex justify-center items-center'>
                 {!ImageHasError ? (
                     <Image
                         source={{ uri: imageUrl }}
@@ -26,6 +28,10 @@ const SongBox = ({ title, artist, imageUrl }: Props) => {
                         <Music2 size={50}></Music2>
                     </View>
                 )}
+                <View className='absolute right-2 bottom-2 flex justify-center items-center
+                    transition-opacity opacity-0 group-hover:opacity-100'>
+                    <AddButton size={35} className='p-1 bg-spotify-green rounded-full' />
+                </View>
             </View>
             <View className='mt-2'>
                 <Text numberOfLines={1} className='text-regular text-md font-bold'>{title}</Text>
